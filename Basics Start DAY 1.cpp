faea9367763c337dc3b8cd991025153e9f2dae7a
MANGLAM TRIPATHI <58903148+manglam-tripathi@users.noreply.github.com>
2020-12-11 14:38:44 +0530

    Add files via upload

---
+++ Basics Start DAY 1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"1) "<<endl;
+	cout<<"*"<<endl;
+	cout<<"**"<<endl;
+	cout<<"***"<<endl;
+	cout<<"****"<<endl;
+	cout<<endl;
+	cout<<"2) ";
+	int x=10;
+	cout<<"The integer is "<<x<<endl;
+	cout<<"3)";
+	int numb;
+	cout<<"Enter a number ";
+	cin>>numb;
+	cout<<"The inputted number is: "<<numb<<endl;
+	cout<<"4)"<<endl;
+	cout<<"****"<<endl;
+	cout<<" ** "<<endl;
+	cout<<" * "<<endl;
+	cout<<" ** "<<endl;
+	cout<<"****"<<endl;
+	cout<<endl<<"5)"<<endl;
+	cout<<"Hello"<<endl;
+	cout<<"CodesDope"<<endl;
+	return 0;
+	
+}
